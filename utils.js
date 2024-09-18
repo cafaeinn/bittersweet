ea1d5911c3321f3082e26d391faa80fe3e5efea8
cafaeinn <itwasnaow@gmail.com>
2024-09-18 23:28:32 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -121,7 +121,7 @@ const ttdl = async(message, args) => {
 	message.reply({
 		files: [{
 			attachment: res.data.data.play,
-			name: 'donglot.webp',
+			name: 'donglot.mp4',
 		}]
 	});
 	} else if(ttValid.test(ttURL) === false) {
@@ -139,7 +139,7 @@ const instadl = async(message, args) => {
 		message.reply({
 			files: [{
 				attachment: res.data[i].url,
-				name: 'donglot.webp'
+				name: 'donglot.mp4'
 			}]
 		});
 	}
